18cc1c470bf505d15c3736145b619d93b6048f4f
Pedro Brando <62448483+Pedro-Brando@users.noreply.github.com>
2024-07-13 14:17:37 -0300

    Update Curriculo - EN.py

--- Curriculo - EN.py
+++ Curriculo - EN.py
@@ -129,7 +129,12 @@ pdf.section_separator()
 pdf.chapter_title('Certifications and Additional Courses:')
 certificacoes = [
     "CCNA: Switching, Routing, and Wireless Essentials - Cisco (Issued in June 2024)",
-    "CCNA: Introduction to Networks - Cisco (Issued in December 2023)"
+    "CCNA: Introduction to Networks - Cisco (Issued in December 2023)",
+    "IT Essentials: PC Hardware and Software - Cisco (Issued in June 2020)",
+    "CSJ: Desenvolvimento Unity - Crie seus Jogos (Isued in July 2024)",
+    "CSJ: Game On (Multiplayer) - Crie seus Jogos (Isued in July 2024)",
+    "CSJ: Game Design - Crie seus Jogos (Isued in July 2024)",
+    "CSJ: Design de Jogos 2D - Crie seus Jogos (Isued in July 2024)",
 ]
 for cert in certificacoes:
     pdf.chapter_body(f"- {cert}")
